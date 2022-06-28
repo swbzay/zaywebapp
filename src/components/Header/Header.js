4347d4e1a1d3889ae715f2952d06e6ea9c49365b
xmmcdani <xavion.mcdaniels@gmail.com>
2022-06-28 00:38:37 -0400

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
@@ -49,6 +49,9 @@ const Header = () =>  (
      <SocialIcons href="https://instagram.com">
          <AiFillInstagram size="3rem" />
      </SocialIcons>
+         <SocialIcons href="https://YouTube.com">
+             <AiFillYoutube size="3rem" />
+         </SocialIcons>
 
      </Div3>
      </Container>
