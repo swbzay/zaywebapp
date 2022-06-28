0e8b674aeb5b77acd755f5dfa9665cf96bab5a62
xmmcdani <xavion.mcdaniels@gmail.com>
2022-06-27 23:36:26 -0400

    push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,13 +27,13 @@ const Header = () =>  (
              </Link>
          </li>
          <li>
-             <Link href="#companies">
-                 <NavLink>Companies</NavLink>
+             <Link href="#about">
+                 <NavLink>About</NavLink>
              </Link>
          </li>
          <li>
-             <Link href="#about">
-                 <NavLink>About</NavLink>
+             <Link href="#companies">
+                 <NavLink>Companies</NavLink>
              </Link>
          </li>
      </Div2>
