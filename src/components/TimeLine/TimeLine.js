8ffbd56b75c53c62029cef13e8852a96cb007286
xmmcdani <xavion.mcdaniels@gmail.com>
2022-07-12 17:32:23 -0400

    push

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
         <SectionTitle>About Me</SectionTitle>
         <SectionText>
-            The purpose of this ETC...
+            Coming Soon...
         </SectionText>
         <CarouselContainer ref={carouselRef} onScroll ={handleScroll}>
             <>
